08205bf1beec66b77ebcf0d74635e6b9ffc725a2
PierreBonsignore <pierre.bonsignore@gmail.com>
2017-01-09 22:50:11 +0100

    xml 9 jan 17
    travail sur xml arme

--- Emprah_project - Copie 090117/DAL/ArmesDAL.cs
+++ Emprah_project - Copie 090117/DAL/ArmesDAL.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DAL
 {
     public class ArmesDAL
     {
+        public void ExportXML(BindingList<Arme> liste, FileStream fluxSortie)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement racine = doc.CreateElement("racine");
+            doc.AppendChild(racine);
+
+            foreach (Arme item in liste)
+            {
+                XmlElement arme = doc.CreateElement("arme");
+                XmlAttribute nom = doc.CreateAttribute("nom");
+                nom.InnerText = item.Nom;
+                arme.Attributes.Append(nom);
+                racine.AppendChild(arme);
+            }
+
+            doc.Save(fluxSortie);
+        }
     }
 }
---
+++ Emprah_project - Copie 090117/IHM/Form1.cs
@@ -0,0 +1,46 @@
+﻿using BO;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IHM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private FileStream SelectionnerSortie()
+        {
+            saveFileDialog.ShowDialog();
+            return (FileStream)saveFileDialog.OpenFile();
+        }
+
+        private void SauverXML()
+        {
+            BindingList<Arme> liste = new BindingList<Arme>();
+            Arme flingue = new Arme();
+            flingue.Nom = "My Big Zooka";
+            liste.Add(flingue);
+            FileStream flux = SelectionnerSortie();
+            new ArmesDAL().ExportXML(liste, flux);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SauverXML();
+        }
+    }
+}
